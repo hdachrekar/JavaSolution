df0af56ab6f70ed17d8a6edf0f18ce2454f2b054
harshavardhan.a <harshavardhan@eclinicalworks.com>
2020-09-19 23:57:58 -0400

    LeetcodeandPramps - WordLadderOrShortestWordEditPathLC127

--- src/LCplusPramps/WordLadderOrShortestWordEditPathLC127.java
+++ src/LCplusPramps/WordLadderOrShortestWordEditPathLC127.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class WordLadderOrShortestWordEditPathLC127 {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if (beginWord.length() != endWord.length()) return -1;
+        if (beginWord.length() != endWord.length()) return 0;
         Set<String> wordSet = new HashSet<String>(wordList);
         if (!wordSet.contains(endWord)) return 0;
         Queue<String> queueNode = new LinkedList<String>();
